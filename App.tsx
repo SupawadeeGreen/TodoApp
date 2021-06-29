07b452db6987e1e5b000da806b07b67d3bb6dab9
Your Name <you@example.com>
2021-06-29 10:39:42 +0700

    edit style

--- App.tsx
+++ App.tsx
@@ -39,15 +39,17 @@ const App = () => {
     );
   };
   return (
-    <>
+    <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
           onChangeText={str => setInput(str)}
-          placeholder="input todo"
+          placeholder="Add todo on your list ..."
           value={input}
         />
-        <Button title="create" onPress={addInput} />
+        <TouchableOpacity onPress={addInput} style={styles.createButton}>
+          <Text style={styles.createText}>Create</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.filterContainer}>
         <FilterComponent
@@ -55,21 +57,21 @@ const App = () => {
             setFilterSelector(0);
           }}
           title="All"
-          color={filterSelected === 0 ? 'blue' : 'black'}
+          color={filterSelected === 0 ? '#2196F3' : '#888'}
         />
         <FilterComponent
           onPress={() => {
             setFilterSelector(1);
           }}
           title="CheckList"
-          color={filterSelected === 1 ? 'blue' : 'black'}
+          color={filterSelected === 1 ? '#2196F3' : '#888'}
         />
         <FilterComponent
           onPress={() => {
             setFilterSelector(2);
           }}
           title="UnCheckList"
-          color={filterSelected === 2 ? 'blue' : 'black'}
+          color={filterSelected === 2 ? '#2196F3' : '#888'}
         />
       </View>
       <FlatList
@@ -89,50 +91,86 @@ const App = () => {
                       value={item.item.status}
                       onValueChange={() => checkHandle(item.item.id)}
                     />
-                    <Text>{item.item.name}</Text>
+                    <Text style={styles.itemText}>{item.item.name}</Text>
                   </View>
                 </TouchableOpacity>
-                <Button
-                  title="delete"
+                <TouchableOpacity
                   onPress={() => {
                     const newMapList = todoList.filter(
                       i => i.id !== item.item.id,
                     );
                     setTodoList(newMapList);
                   }}
-                />
+                  style={styles.deleteButton}>
+                  <Text style={styles.createText}>Delete</Text>
+                </TouchableOpacity>
               </View>
             );
           }
         }}
       />
-      <Button
-        title="Delete All"
-        onPress={() => {
-          setTodoList([]);
-        }}
-      />
-    </>
+      {todoList.length > 0 && (
+        <Button
+          color="#b82222"
+          title="Delete All"
+          onPress={() => {
+            setTodoList([]);
+          }}
+        />
+      )}
+    </View>
   );
 };
 const styles = StyleSheet.create({
+  screen: {
+    padding: 15,
+    flex: 1,
+  },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
+    borderRadius: 13,
+    paddingHorizontal: 15,
+    borderColor: '#888',
     flex: 1,
   },
   inputContainer: {
     flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   list: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
   filterContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     margin: 20,
+  },
+  createButton: {
+    height: 30,
+    width: 60,
+    backgroundColor: '#2196F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  createText: {
+    color: 'white',
+  },
+  itemText: {
+    color: '#5a5a5a',
+  },
+  deleteButton: {
+    height: 30,
+    width: 60,
+    backgroundColor: '#b82222',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
   },
 });
 export default App;
